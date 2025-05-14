7bcbbeb46628d752a65a5787564b6ba40092ac8a
tmsoft12 <tmsoft12@gmail.com>
2025-05-14 09:58:24 +0500

    oki

--- internal/config/config.go
+++ internal/config/config.go
@@ -8,7 +8,7 @@ type Config struct {
 
 func Load() *Config {
 	return &Config{
-		MQTTBroker:  "tcp://localhost:1883",
+		MQTTBroker:  "tcp://192.168.100.220:1883",
 		ServerPort:  ":3000",
-		DatabaseURL: "postgres://tmsoft:12@127.0.0.1:5432/server"}
+		DatabaseURL: "postgres://tmsoft:12@192.168.100.220:5432/server"}
 }
